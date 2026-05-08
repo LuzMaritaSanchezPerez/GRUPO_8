9f63f044d0aa0438520e09445b676e3833ede6ed
day130725 <N00439638@upn.pe>
2026-05-08 00:48:20 -0500

    PrimerCommit
    PrimerCommit

---
+++ EstudianteControlador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlestudiante;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author UCC20300
+ */
+public class EstudianteControlador {
+    ArrayList<Estudiante> stds = new ArrayList();
+    public void agregar(Estudiante nuevo_estudiante){
+        stds.add(nuevo_estudiante);
+    }
+    public void listar(){
+        for(int i=0; i<stds.size();i++){
+            stds.get(i).verDatos();
+        }
+    }
+}
